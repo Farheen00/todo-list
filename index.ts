3874fa35c32ac8cd37b859d8e33294a8100e4ec5
Mehreen Osama <Mehreen@PGL.COM>
2024-04-27 01:11:11 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer"
+let todos =[];
+let condition = true;
+while(condition) {
+    let addTodos=await inquirer.prompt(
+        [
+            {
+                name:"QuestionOne",
+                type:"input",
+                message:"What would you like to add in your todos?"
+            },
+            {
+                name:"QuestionTwo",
+                type:"confirm",
+                message:"Do your want to add more to your todos?",
+                default:"true"
+            }
+        ]
+    );
+
+todos.push(addTodos.QuestionOne)
+condition =addTodos.QuestionTwo
+console.log(todos)
+
+}
